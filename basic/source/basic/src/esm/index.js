685b27b961a7985c915dee9036cfac9d22df4a85
w-t-w <15866369958@163.com>
2026-02-15 20:59:10 +0800

    accumulation: think of nodejs/think of daily-problems

--- basic/source/basic/src/esm/index.js
+++ basic/source/basic/src/esm/index.js
@@ -1,14 +1,1 @@
 require("./src/index.mjs");
-
-class Demo {
-  constructor(name) {
-    this.name = name;
-  }
-
-  run() {
-    console.log("Demo run:", this.name);
-  }
-}
-
-const demo = new Demo("esm");
-demo.run();
